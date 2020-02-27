e1da64ce1d617f99dd1f00a0230c4ea01fd92a0c
Sebastian Vogt <betont.online@googlemail.com>
2020-02-27 22:17:13 +0100

    change names

--- CustomEvent/Program.vb
+++ CustomEvent/Program.vb
@@ -4,33 +4,33 @@ Imports CustomEvent.Sevo
 
 Module Program
 
-    Dim WithEvents c As Counter = New Counter
+    Dim WithEvents MyCounter As Counter = New Counter
 
     Sub Main(args As String())
-        AddHandler c.StartEvent, AddressOf c_StartEvent
+        AddHandler MyCounter.StartEvent, AddressOf MyCounter_StartEvent
         'RemoveHandler c.StartEvent, AddressOf c_StartEvent
-        c.Run()
+        MyCounter.Run()
 
     End Sub
 
 
-    Sub c_StartEvent(sender As Object, args As CounterEventArgs)
+    Sub MyCounter_StartEvent(sender As Object, args As CounterEventArgs)
         Dim s As Counter = CType(sender, Counter)
         Console.WriteLine("START_EVENT Count = " & s.Count)
         Console.WriteLine("START_EVENT CountValue (from CounterEventsArgs) = " & args.CountValue)
     End Sub
 
-    'Sub c_StartEvent(sender As Object, args As CounterEventArgs) Handles c.StartEvent
+    'Sub MyCounter_StartEvent(sender As Object, args As CounterEventArgs) Handles MyCounter.StartEvent
     '    Dim s As Counter = CType(sender, Counter)
     '    Console.WriteLine("START_EVENT Count = " & s.Count)
     'End Sub
 
-    Sub c_ChangeEvent(sender As Object, args As CounterEventArgs) Handles c.ChangeEvent
+    Sub MyCounter_ChangeEvent(sender As Object, args As CounterEventArgs) Handles MyCounter.ChangeEvent
         Dim s As Counter = CType(sender, Counter)
         Console.WriteLine("CHANGE_EVENT Count = " & s.Count)
     End Sub
 
-    Sub c_FinishEvent(sender As Object, args As CounterEventArgs) Handles c.FinishEvent
+    Sub MyCounter_FinishEvent(sender As Object, args As CounterEventArgs) Handles MyCounter.FinishEvent
         Dim s As Counter = CType(sender, Counter)
         Console.WriteLine("FINISH_EVENT Count = " & s.Count)
     End Sub
